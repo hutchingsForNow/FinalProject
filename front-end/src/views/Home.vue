6764e94f39defa3f0bc9c9578151185be13d2607
Reagan Hutchings <reaganbh@byu.edu>
2021-04-19 10:36:55 -0600

    copied lab 5

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="home">
+  <image-gallery :photos="photos" />
+  <p v-if="error">{{error}}</p>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+import ImageGallery from '@/components/ImageGallery.vue';
+export default {
+  name: 'Home',
+  components: {
+    ImageGallery,
+  },
+  created() {
+    this.getPhotos();
+  },
+  data() {
+    return {
+      photos: [],
+      error: '',
+    }
+  },
+  methods: {
+    async getPhotos() {
+      try {
+        let response = await axios.get("/api/photos/all");
+        this.photos = response.data;
+      } catch (error) {
+        this.error = error.response.data.message;
+      }
+    },
+  }
+}
+</script>
---
+++ front-end/src/views/Photo.vue
@@ -0,0 +1,28 @@
+
+<template>
+<div class="singlePhoto">
+  <router-link :to="{ name: 'photo', params: { id: photo._id }}"><img :src="photo.path" /></router-link>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return{
+      photo: {}
+    }
+  },
+  async created() {
+  try {
+        let response = await axios.get('/api/photo/:id');
+        this.$route.params.id = response.data.id;
+      } catch (error) {
+        this.$route.params.id = null;
+      }
+    
+  }
+}
+
+</script>
+
